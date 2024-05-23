d83b067cfb420b0836ab081d79c3494970934a67
k9yuw <gyuwoneee@gmail.com>
2024-05-23 18:42:12 +0900

    initialize

---
+++ src/main/java/taka/takaspring/TakaSpringApplication.java
@@ -0,0 +1,13 @@
+package taka.takaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/taka/takaspring/TakaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package taka.takaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
