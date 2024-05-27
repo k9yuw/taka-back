266b82d773e3ee5ea9e3621084f570e4f9582e7d
k9yuw <gyuwoneee@gmail.com>
2024-05-27 16:02:34 +0900

    initialize

--- src/main/java/taka/takaspring/TakaSpringApplication.java
+++ src/main/java/taka/takaspring/TakaSpringApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TakaSpringApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TakaSpringApplication.class, args);
 	}
-
 }
