1be2bca20e396cea336e624587a0882d5233473b
k9yuw <gyuwoneee@gmail.com>
2024-07-04 02:44:39 +0900

    UserRepositoryTest 코드 추가

--- src/main/java/taka/takaspring/Member/jwt/PrincipalDetailsService.java
+++ src/main/java/taka/takaspring/Member/jwt/PrincipalDetailsService.java
@@ -1,9 +1,12 @@
 package taka.takaspring.Member.jwt;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import taka.takaspring.Member.controller.AuthController;
 import taka.takaspring.Member.db.UserEntity;
 import taka.takaspring.Member.db.UserRepository;
 
@@ -11,6 +14,7 @@ import taka.takaspring.Member.db.UserRepository;
 public class PrincipalDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
     public PrincipalDetailsService(UserRepository userRepository) {
 
@@ -25,6 +29,7 @@ public class PrincipalDetailsService implements UserDetailsService {
         try {
             // DB에서 조회
             UserEntity user = userRepository.findByEmail(email);
+            logger.info("////////////리포지토리에서 찾은 Email : {}", user.getEmail());
 
             if (user == null) {
                 // 사용자를 찾지 못한 경우 UsernameNotFoundException을 던진다.
