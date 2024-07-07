5387b7bf6030538157823ceaa666170c94707d93
k9yuw <gyuwoneee@gmail.com>
2024-07-08 03:16:15 +0900

    RentalItemManageService 수정

--- src/main/java/taka/takaspring/Rental/dto/RentalItemManageDto.java
+++ src/main/java/taka/takaspring/Rental/dto/RentalItemManageDto.java
@@ -1,6 +1,9 @@
 package taka.takaspring.Rental.dto;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import taka.takaspring.Organization.db.OrgEntity;
 
 public class RentalItemManageDto {
 
@@ -10,19 +13,31 @@ public class RentalItemManageDto {
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
     public static class RentalItemManageRequest {
 
+        private String orgId;
+
+        @NotBlank(message = "물품 이름을 입력해주세요.")
+        private String itemName;
+
+        private boolean isAvailable;
+
+        private String rentalPeriod;
+
+        private String itemImageUrl;
 
     }
 
 
     @Getter
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @Builder
     public static class RentalItemManageResponse {
 
+        private String itemName;
+        private String rentalPeriod;
 
-
-        @Builder
-        public RentalItemRegiByOrgAdminResponse(){
-
+        public RentalItemManageResponse(String itemName, String rentalPeriod){
+            this.itemName = itemName;
+            this.rentalPeriod = rentalPeriod;
         }
     }
 
