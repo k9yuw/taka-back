7423a965c36909aa8d4e974a71ff5627f757769c
k9yuw <gyuwoneee@gmail.com>
2024-07-08 03:21:31 +0900

    RentalItemManageController 추가

--- src/main/java/taka/takaspring/Rental/controller/RentalItemManageController.java
+++ src/main/java/taka/takaspring/Rental/controller/RentalItemManageController.java
@@ -1,4 +1,45 @@
 package taka.takaspring.Rental.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import taka.takaspring.Rental.db.RentalItemEntity;
+import taka.takaspring.Rental.dto.RentalItemManageDto;
+import taka.takaspring.Rental.service.RentalItemManageService;
+
+@RestController
+@RequestMapping("/api/admin/{organization_id}/items")
 public class RentalItemManageController {
+
+    private final RentalItemManageService rentalItemManageService;
+
+    @Autowired
+    public RentalItemManageController(RentalItemManageService rentalItemManageService) {
+        this.rentalItemManageService = rentalItemManageService;
+    }
+
+    @PostMapping
+    public ResponseEntity<RentalItemManageDto.RentalItemManageResponse> addRentalItem(
+            @PathVariable("organization_id") Long orgId,
+            @RequestBody RentalItemManageDto.RentalItemManageRequest request) {
+        RentalItemManageDto.RentalItemManageResponse response = rentalItemManageService.addRentalItem(orgId, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("/{item_id}")
+    public ResponseEntity<RentalItemEntity> updateRentalItem(
+            @PathVariable("organization_id") Long orgId,
+            @PathVariable("item_id") Long itemId,
+            @RequestBody RentalItemManageDto.RentalItemManageRequest request) {
+        RentalItemEntity rentalItem = rentalItemManageService.updateRentalItem(orgId, itemId, request);
+        return ResponseEntity.ok(rentalItem);
+    }
+
+    @DeleteMapping("/{item_id}")
+    public ResponseEntity<Void> deleteRentalItem(
+            @PathVariable("organization_id") Long orgId,
+            @PathVariable("item_id") Long itemId) {
+        rentalItemManageService.deleteRentalItem(orgId, itemId);
+        return ResponseEntity.noContent().build();
+    }
 }
