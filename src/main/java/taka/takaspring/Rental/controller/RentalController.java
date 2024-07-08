7bf945bba0b3ba7714eb8b5c0774eeaa32bc5e40
k9yuw <gyuwoneee@gmail.com>
2024-07-08 17:47:04 +0900

    물품 대여 로직 컨트롤러에 추가

--- src/main/java/taka/takaspring/Rental/controller/RentalController.java
+++ src/main/java/taka/takaspring/Rental/controller/RentalController.java
@@ -1,4 +1,28 @@
 package taka.takaspring.Rental.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import taka.takaspring.Rental.dto.RentalDto;
+import taka.takaspring.Rental.service.RentalService;
+
+@RestController
+@RequestMapping("/api/rentals")
 public class RentalController {
+
+    private final RentalService rentalService;
+
+    @Autowired
+    public RentalController(RentalService rentalService) {
+        this.rentalService = rentalService;
+    }
+
+    @PostMapping("/{organization_id}/{item_id}")
+    public ResponseEntity<RentalDto.RentalResponse> rentItem(@RequestBody RentalDto.RentalRequest request,
+                                                             @PathVariable("organization_id") Long orgId,
+                                                             @PathVariable("item_id") Long itemId) {
+        RentalDto.RentalResponse response = rentalService.rentItem(request);
+        return ResponseEntity.ok(response);
+    }
+
 }
