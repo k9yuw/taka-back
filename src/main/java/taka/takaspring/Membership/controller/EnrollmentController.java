0f8c94295805db18913b56c475693775ee77cee0
k9yuw <gyuwoneee@gmail.com>
2024-07-11 21:15:52 +0900

    pending 중인 유저들 조회하는 컨트롤러 추가

--- src/main/java/taka/takaspring/Membership/controller/EnrollmentController.java
+++ src/main/java/taka/takaspring/Membership/controller/EnrollmentController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import taka.takaspring.Membership.dto.EnrollmentDto;
+import taka.takaspring.Membership.dto.MembershipDto;
 import taka.takaspring.Membership.service.EnrollmentService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/enrollments")
@@ -28,19 +31,23 @@ public class EnrollmentController {
     }
 
     // 입부 신청한 회원 목록 조회
-    @GetMapping("/admin/membership/{orgId}/enrollment")
+    @GetMapping("/admin/membership/{organization_id}/enrollment")
+    public ResponseEntity<List<EnrollmentDto.EnrollmentIntermediateRequest>> getPendingUsers(@PathVariable("organization_id") Long orgId) {
+        List<EnrollmentDto.EnrollmentIntermediateRequest> pendingUserList = enrollmentService.getPendingUsers(orgId);
+        return ResponseEntity.ok(pendingUserList);
+    }
 
     // 입부 승인
-    @PatchMapping("/admin/membership/{membershipId}/approve")
-    public ResponseEntity<EnrollmentDto.EnrollmentResponse> approveEnrollment(@PathVariable Long membershipId,
+    @PatchMapping("/admin/membership/{membership_id}/approve")
+    public ResponseEntity<EnrollmentDto.EnrollmentResponse> approveEnrollment(@PathVariable Long membership_id,
                                                                               @RequestBody EnrollmentDto.EnrollmentIntermediateRequest request) {
         EnrollmentDto.EnrollmentResponse response = enrollmentService.approveEnrollment(request);
         return ResponseEntity.ok(response);
     }
 
     // 입부 거절
-    @PatchMapping("/admin/membership/{membershipId}/reject")
-    public ResponseEntity<EnrollmentDto.EnrollmentResponse> rejectEnrollment(@PathVariable Long membershipId,
+    @PatchMapping("/admin/membership/{membership_id}/reject")
+    public ResponseEntity<EnrollmentDto.EnrollmentResponse> rejectEnrollment(@PathVariable Long membership_id,
                                                                              @RequestBody EnrollmentDto.EnrollmentIntermediateRequest request) {
         EnrollmentDto.EnrollmentResponse response = enrollmentService.rejectEnrollment(request);
         return ResponseEntity.ok(response);
