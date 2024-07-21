3056080fd9d50251c82f64b6a5a1fb68b7bca2da
k9yuw <gyuwoneee@gmail.com>
2024-07-21 19:54:55 +0900

    membership/enrollment 로직 분리

--- src/main/java/taka/takaspring/Membership/service/MembershipService.java
+++ src/main/java/taka/takaspring/Membership/service/MembershipService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import taka.takaspring.Membership.db.MembershipEntity;
 import taka.takaspring.Membership.db.MembershipRepository;
+import taka.takaspring.Membership.dto.EnrollmentDto;
 import taka.takaspring.Membership.dto.MembershipDto;
 
 import java.util.List;
@@ -50,5 +51,60 @@ public class MembershipService {
         } else {
             throw new EntityNotFoundException("단체에 해당 회원이 존재하지 않습니다.");
         }
+    }
+
+
+    @Transactional
+    public EnrollmentDto.EnrollmentResponse approveEnrollment(EnrollmentDto.EnrollmentIntermediateRequest request){
+
+        Long membershipId = request.getMembership().getId();
+        MembershipEntity membership = membershipRepository.findById(membershipId)
+                .orElseThrow(() -> new RuntimeException("존재하지 않는 요청입니다."));
+
+        membership.setStatus(MembershipEntity.MembershipStatus.APPROVED);
+        membershipRepository.save(membership);
+
+        EnrollmentDto.EnrollmentResponse response = EnrollmentDto.EnrollmentResponse.builder().
+                userName(membership.getUser().getName()).
+                orgName(membership.getOrg().getOrgName()).
+                build();
+
+        return response;
+    }
+
+    @Transactional
+    public EnrollmentDto.EnrollmentResponse rejectEnrollment(EnrollmentDto.EnrollmentIntermediateRequest request){
+
+        Long membershipId = request.getMembership().getId();
+        MembershipEntity membership = membershipRepository.findById(membershipId)
+                .orElseThrow(() -> new RuntimeException("존재하지 않는 요청입니다."));
+
+        membership.setStatus(MembershipEntity.MembershipStatus.REJECTED);
+        membershipRepository.save(membership);
+
+        EnrollmentDto.EnrollmentResponse response = EnrollmentDto.EnrollmentResponse.builder().
+                userName(membership.getUser().getName()).
+                orgName(membership.getOrg().getOrgName()).
+                build();
+
+        return response;
+
+    }
+
+    @Transactional
+    public List<EnrollmentDto.EnrollmentIntermediateRequest> getPendingUsers(Long organizationId) {
+        List<MembershipEntity> membershipEntities = membershipRepository.findByOrgIdAndStatus(organizationId, MembershipEntity.MembershipStatus.PENDING);
+        return membershipEntities.stream()
+                .map(this::convertToEnrollmentIntermediateRequest)
+                .collect(Collectors.toList());
+    }
+
+    private EnrollmentDto.EnrollmentIntermediateRequest convertToEnrollmentIntermediateRequest(MembershipEntity membershipEntity) {
+
+        EnrollmentDto.EnrollmentIntermediateRequest response = EnrollmentDto.EnrollmentIntermediateRequest.builder().
+                membership(membershipEntity).
+                build();
+
+        return response;
     }
 }
