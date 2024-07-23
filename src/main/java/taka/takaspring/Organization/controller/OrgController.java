27abccd769bc58137b50e4e01df8b0bcc52bd59f
k9yuw <gyuwoneee@gmail.com>
2024-07-23 18:16:02 +0900

    슈퍼어드민이 특정 단체의 관리자 지정하는 로직 추가

--- src/main/java/taka/takaspring/Organization/controller/OrgController.java
+++ src/main/java/taka/takaspring/Organization/controller/OrgController.java
@@ -9,34 +9,44 @@ import taka.takaspring.Organization.dto.OrgDto;
 import taka.takaspring.Organization.service.OrgService;
 
 @RestController
-@RequestMapping("/api/super_admin")
+@RequestMapping("/api/super_admin/{org_id}")
 @RequiredArgsConstructor
 public class OrgController {
 
     private final OrgService orgService;
 
+    // 슈퍼어드민이 단체 추가
     @PostMapping
     public ResponseEntity<OrgDto.OrgResponse> createOrg(@RequestBody @Valid OrgDto.OrgRequest request) {
         OrgDto.OrgResponse createdOrg = orgService.registerOrg(request);
         return ResponseEntity.ok().body(createdOrg);
     }
 
-    @GetMapping("/{org_id}")
+    // 슈퍼어드민이 단체 조회
+    @GetMapping
     public ResponseEntity<OrgDto.OrgResponse> getOrg(@PathVariable Long orgId) {
         OrgDto.OrgResponse response = orgService.getOrg(orgId);
         return ResponseEntity.ok(response);
     }
 
-    @PatchMapping("/{org_id}")
+    // 슈퍼어드민이 단체 정보 수정
+    @PatchMapping
     public ResponseEntity<OrgEntity> updateOrg(@PathVariable Long orgId, @RequestBody OrgDto.OrgRequest request) {
         OrgEntity updatedOrg = orgService.updateOrg(orgId, request);
         return ResponseEntity.ok(updatedOrg);
     }
 
-    @DeleteMapping("/{org_id}")
+    // 슈퍼어드민이 단체 삭제
+    @DeleteMapping
     public ResponseEntity<Void> deleteOrg(@PathVariable Long org_id) {
         orgService.deleteOrg(org_id);
         return ResponseEntity.noContent().build();
     }
 
+    // 슈퍼어드민이 특정 단체의 admin 지정
+    @PostMapping("/designate_admin")
+    public ResponseEntity<Void> designateAdmin(@RequestParam Long superadminId, @RequestParam Long userId, @RequestParam Long orgId) {
+        orgService.designateAdmin(superadminId, userId, orgId);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/taka/takaspring/Organization/service/OrgService.java
+++ src/main/java/taka/takaspring/Organization/service/OrgService.java
@@ -3,18 +3,30 @@ package taka.takaspring.Organization.service;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import taka.takaspring.Member.db.UserEntity;
+import taka.takaspring.Membership.db.MembershipEntity;
+import taka.takaspring.Membership.db.MembershipRepository;
+import taka.takaspring.Membership.exception.MembershipNotFoundException;
 import taka.takaspring.Organization.db.OrgEntity;
 import taka.takaspring.Organization.db.OrgRepository;
 import taka.takaspring.Organization.dto.OrgDto;
 
 import java.util.Optional;
 
+@PreAuthorize("hasRole('SUPER_ADMIN')")
 @Service
 public class OrgService {
 
     @Autowired
-    private OrgRepository orgRepository;
+    private final OrgRepository orgRepository;
+    private final MembershipRepository membershipRepository;
+
+    public OrgService(OrgRepository orgRepository, MembershipRepository membershipRepository){
+        this.orgRepository = orgRepository;
+        this.membershipRepository = membershipRepository;
+    }
 
     @Transactional
     public OrgDto.OrgResponse registerOrg(OrgDto.OrgRequest request) {
@@ -50,17 +62,13 @@ public class OrgService {
                 build();
 
         return response;
-
     }
 
     @Transactional
     public OrgEntity updateOrg(Long orgId, OrgDto.OrgRequest request) {
-
         OrgEntity updateOrg = orgRepository.findById(orgId)
                 .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 단체입니다."));
-
         updateOrg.updateFields(request.getDepartment(), request.getOrgDescription());
-
         return orgRepository.save(updateOrg);
     }
 
@@ -69,5 +77,17 @@ public class OrgService {
         Optional<OrgEntity> orgEntityOptional = orgRepository.findById(orgId);
         OrgEntity orgEntity = orgEntityOptional.get();
         orgRepository.delete(orgEntity);
+    }
+
+    // 슈퍼어드민이 특정 단체의 관리자 지정
+    @Transactional
+    public void designateAdmin(Long superadminId, Long userId, Long orgId) {
+
+        MembershipEntity membership = membershipRepository.findByOrgIdAndUserId(orgId, userId)
+                .orElseThrow(() -> new MembershipNotFoundException("존재하지 않는 멤버십 - user id: " + userId + " and org id: " + orgId));
+
+        // Admin 권한 부여
+        membership.designateAdmin(true);
+        membershipRepository.save(membership);
     }
 }
