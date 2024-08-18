23436683253ed44dfd8475c5a428536e394aba40
k9yuw <gyuwoneee@gmail.com>
2024-08-18 21:03:55 +0900

    Dockerfile 추가

--- src/main/java/taka/takaspring/TakaSpringApplication.java
+++ src/main/java/taka/takaspring/TakaSpringApplication.java
@@ -1,11 +1,21 @@
 package taka.takaspring;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TakaSpringApplication {
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		System.setProperty("SPRING_MAIL_USERNAME", dotenv.get("SPRING_MAIL_USERNAME"));
+		System.setProperty("SPRING_MAIL_PASSWORD", dotenv.get("SPRING_MAIL_PASSWORD"));
+		System.setProperty("SPRING_JWT_SECRET", dotenv.get("SPRING_JWT_SECRET"));
+
 		SpringApplication.run(TakaSpringApplication.class, args);
 	}
 }
