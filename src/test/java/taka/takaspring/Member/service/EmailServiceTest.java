bec94fd984ff6d5b576597e72608c9fb1b992968
k9yuw <gyuwoneee@gmail.com>
2024-09-08 16:43:45 +0900

    테스트코드 주석처리

--- src/test/java/taka/takaspring/Member/service/EmailServiceTest.java
+++ src/test/java/taka/takaspring/Member/service/EmailServiceTest.java
@@ -1,45 +1,45 @@
-package taka.takaspring.Member.service;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
-
-class EmailServiceTest {
-
-    @Mock
-    private JavaMailSender emailSender;
-
-    @InjectMocks
-    private EmailService emailService;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
-
-    @Test
-    void sendSimpleMessage_shouldSendEmail() {
-        String to = "choki@korea.ac.kr";
-        String subject = "Test Subject";
-        String text = "Test ㅠody";
-
-        emailService.sendSimpleMessage(to, subject, text);
-
-        // ArgumentCaptor이 메서드 호출 시 전달된 인자를 캡처하여 검증할 수 있게 해준다.
-        ArgumentCaptor<SimpleMailMessage> messageCaptor = ArgumentCaptor.forClass(SimpleMailMessage.class);
-        verify(emailSender).send(messageCaptor.capture());
-
-        SimpleMailMessage capturedMessage = messageCaptor.getValue();
-        assertEquals(to, capturedMessage.getTo()[0]);
-        assertEquals(subject, capturedMessage.getSubject());
-        assertEquals(text, capturedMessage.getText());
-    }
-}
+//package taka.takaspring.Member.service;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.ArgumentCaptor;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.MockitoAnnotations;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.mockito.Mockito.verify;
+//
+//class EmailServiceTest {
+//
+//    @Mock
+//    private JavaMailSender emailSender;
+//
+//    @InjectMocks
+//    private EmailService emailService;
+//
+//    @BeforeEach
+//    void setUp() {
+//        MockitoAnnotations.openMocks(this);
+//    }
+//
+//    @Test
+//    void sendSimpleMessage_shouldSendEmail() {
+//        String to = "choki@korea.ac.kr";
+//        String subject = "Test Subject";
+//        String text = "Test body";
+//
+//        emailService.sendSimpleMessage(to, subject, text);
+//
+//        // ArgumentCaptor이 메서드 호출 시 전달된 인자를 캡처하여 검증할 수 있게 해준다.
+//        ArgumentCaptor<SimpleMailMessage> messageCaptor = ArgumentCaptor.forClass(SimpleMailMessage.class);
+//        verify(emailSender).send(messageCaptor.capture());
+//
+//        SimpleMailMessage capturedMessage = messageCaptor.getValue();
+//        assertEquals(to, capturedMessage.getTo()[0]);
+//        assertEquals(subject, capturedMessage.getSubject());
+//        assertEquals(text, capturedMessage.getText());
+//    }
+//}
